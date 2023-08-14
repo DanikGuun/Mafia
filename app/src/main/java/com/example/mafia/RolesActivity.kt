bd696e763f3185a708216f03977ccf81baf43aa2
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2023-08-14 18:59:43 +0500

    Генерация ролей2
    Сделал генерацию плашек в ролях, забыл добавить файлы в индекс

--- app/src/main/java/com/example/mafia/RolesActivity.kt
+++ app/src/main/java/com/example/mafia/RolesActivity.kt
@@ -3,14 +3,50 @@ package com.example.mafia
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.example.mafia.roles.Roles.*
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.ConstraintSet.BOTTOM
+import androidx.constraintlayout.widget.ConstraintSet.Constraint
+import androidx.constraintlayout.widget.ConstraintSet.LEFT
+import androidx.constraintlayout.widget.ConstraintSet.RIGHT
+import androidx.constraintlayout.widget.ConstraintSet.TOP
+import com.example.mafia.roles.Roles
+import com.google.android.material.bottomappbar.BottomAppBar
 
 class RolesActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.roles_activity)
+        generateRolesPlanks()
     }
-    
+    private fun generateRolesPlanks(){
+        var count = 0
+        val baseID = 1000
+        for (role in Roles.getAllRoles()){
+            val roleLayout = LinearLayout(this)
+            roleLayout.setBackgroundResource(R.drawable.role_bar_shape)
+            roleLayout.id = baseID + count
+
+            val mainConstraint = findViewById<ConstraintLayout>(R.id.role_constraint)
+            mainConstraint.addView(roleLayout, ViewGroup.LayoutParams(400, 200))
+
+            val constraintSet = ConstraintSet()
+            constraintSet.clone(mainConstraint)
+
+
+            if (count == 0)  constraintSet.connect(roleLayout.id, TOP, R.id.role_toolbar, BOTTOM, 40)
+            else constraintSet.connect(roleLayout.id, TOP, baseID+count-1, BOTTOM, 20)
+            constraintSet.connect(roleLayout.id, LEFT, mainConstraint.id, LEFT)
+            constraintSet.connect(roleLayout.id, RIGHT, mainConstraint.id, RIGHT)
+            constraintSet.applyTo(mainConstraint)
+
+            count++
+        }
+    }
 }
--- app/src/main/java/com/example/mafia/roles/Roles.kt
+++ app/src/main/java/com/example/mafia/roles/Roles.kt
@@ -1,6 +1,9 @@
 package com.example.mafia.roles
 
-class Roles {
+object Roles {
+    fun getAllRoles(): Array<Role>{
+        return arrayOf(Peaceful(), Mafia(), Doctor(), Sherif(), Prostitute(), Avenger(), Journalist(), Kamikaze())
+    }
     class Peaceful: Role{
         override val name = "Мирный житель"
             get() = field
@@ -12,5 +15,70 @@ class Roles {
 
         }
 
+    }
+    class Mafia: Role{
+        override val name = "Мафия"
+            get() = field
+        override val description = "Цель - убить всех мирных"
+            get() = field
+
+        override fun action() {
+        }
+    }
+    class Doctor: Role{
+        override val name = "Доктор"
+            get() = field
+        override val description = "Лечит"
+            get() = field
+
+        override fun action() {
+        }
+    }
+    class Sherif: Role{
+        override val name = "Комиссар"
+            get() = field
+        override val description = "Ищет Мафию"
+            get() = field
+
+        override fun action() {
+        }
+    }
+    class Prostitute: Role {
+        override val name = "Путана"
+            get() = field
+        override val description = "чпокается"
+            get() = field
+
+        override fun action() {
+            TODO("Not yet implemented")
+        }
+    }
+    class Avenger: Role{
+        override val name = "Мститель"
+            get() = field
+        override val description = "Убивает"
+            get() = field
+
+        override fun action() {
+
+        }
+    }
+    class Journalist: Role{
+        override val name = "Журналист"
+            get() = field
+        override val description = "Смотрит в одной ли команде игроки"
+            get() = field
+
+        override fun action() {
+        }
+    }
+    class Kamikaze: Role{
+        override val name = "Камикадзе"
+            get() = field
+        override val description = "Убивает на голосовании"
+            get() = field
+
+        override fun action() {
+        }
     }
 }
