c24fe3a392b6190d4b12e4b50f074b005bef4386
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2023-08-18 14:01:36 +0500

    сделал алерт с добавлением игрока
    Пока пустой

--- app/src/main/java/com/example/mafia/GameSettingsActivity.kt
+++ app/src/main/java/com/example/mafia/GameSettingsActivity.kt
@@ -2,11 +2,32 @@ package com.example.mafia
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 
 class GameSettingsActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.game_start_activity)
+        findViewById<Button>(R.id.player_add_button).setOnClickListener {onAddPlayerButton()}
+    }
+    private fun onAddPlayerButton(){
+        val alertLayout = layoutInflater.inflate(R.layout.add_player_alert, null)
+        val alertBuilder = AlertDialog.Builder(this)
+        alertBuilder.setView(alertLayout)
+        val alert = alertBuilder.create()
+
+        alertLayout.findViewById<Button>(R.id.accept_player)?.setOnClickListener {addPlayer(alert)}
+        alertLayout.findViewById<Button>(R.id.cancel_player)?.setOnClickListener {cancelPlayer(alert)}
+        alert.show()
+    }
+    private fun addPlayer(alert: AlertDialog){
+        Log.d("TAG", "addPlayer: 123")
+        alert.cancel()
+    }
+    private fun cancelPlayer(alert: AlertDialog){
+        alert.cancel()
     }
 }
